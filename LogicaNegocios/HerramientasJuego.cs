f0fefea6065c2e14fb6097ec5c425529993c190f
Henry <henry.arroyo@ulatina.net>
2016-10-17 10:36:32 -0700

    Se modificaron los metodos de ayuda (HerramientasJuego) para ser mas genericos y con el tipo de dato correspondiente

--- LogicaNegocios/HerramientasJuego.cs
+++ LogicaNegocios/HerramientasJuego.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace LogicaNegocios
 {
-    public static class HerramientasJuego
+    public class HerramientasJuego
     {
+
+        static string[] NombresDeColumnas = new string[] { "B", "I", "N", "G", "O" };
+
         public static bool sonCartonesIguales(CartonBingo carton1, CartonBingo carton2)
         {
           //  int[] array; 
             for (int i=0; i<5; i++)
             {
-              //  if(carton1.getCarton().Values.[i] != carton2.getCarton().Values[i])
+                //if(carton1.getCarton().Values.[i] != carton2.getCarton().Values[i])
                // {
                //     return false;
               //  }
@@ -21,12 +24,49 @@ namespace LogicaNegocios
             return true;;
         }
 
-        public static string encontrarColumna(int numero, int totalNumeros)
+        public static string[] GetNombresDeColumnas()
         {
-            Dictionary<string, int[]> carton = new CartonBingo(totalNumeros).getCarton();
-            List<string> listaDeKeys = new List<string>(carton.Keys);
-            return listaDeKeys[(int)Math.Floor((double)((numero - 1) / (totalNumeros/5)))];
+            return NombresDeColumnas;
         }
+
+        public static string EncontrarAQueColumnaPertenece(int numero, int intervalo)
+        {
+            return NombresDeColumnas[(int)Math.Floor((double)((numero - 1) / intervalo))];
+        }
+
+        public static int[] EncontrarIntervaloDeColumna(string columna, int intervalo)
+        {
+            int indice = Array.IndexOf(NombresDeColumnas, columna);
+            int minimo = intervalo * indice + 1;
+            int maximo = intervalo * indice + intervalo;
+            return new int[] { minimo, maximo };
+        }
+
+        public static CampoCarton[] GenerarColumnaBingo(string columna, int intervalo)
+        {
+            int[] valoresColumna = Herramientas.CrearArray.CrearRandomArray(5,
+                                                                EncontrarIntervaloDeColumna(columna, intervalo)[0],
+                                                                EncontrarIntervaloDeColumna(columna, intervalo)[1]);
+            CampoCarton[] campos = new CampoCarton[5];
+            for (int i=0; i<valoresColumna.Length; i++)
+            {
+                campos[i] = new CampoCarton(valoresColumna[i], i, columna);
+            }
+            return campos;
+
+        }
+
+        public static string TempImprimirColumna(CampoCarton[] columna)
+        {
+            var msg = "";
+            foreach (var item in columna)
+            {
+                msg += item.ToString() + ",";
+            }
+            return msg;
+        }
+
+
 
     }
 
