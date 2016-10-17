f992fd9fb2d416a2e0592a3f32450c4562839197
Henry <henry.arroyo@ulatina.net>
2016-10-17 03:18:02 -0700

    Se agrego el proyecto Herramientas con las clases ConsultarArray, CrearArray y Modificar Array

---
+++ Herramientas/ConsultarArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herramientas
+{
+    public class ConsultarArray
+    {
+        public static bool estaElNumeroEnArray(int[] elArray, int elNumeroAConsultar)
+        {
+            // TO DO
+            return false;
+        }
+
+        public static string imprimirArray(int[] elArray)
+        {
+            return string.Join(",", elArray);
+        }
+
+        public static Boolean sonArraysIguales(int[] array1, int[] array2)
+        {
+            return array1.SequenceEqual(array2);
+        }
+    }
+}
