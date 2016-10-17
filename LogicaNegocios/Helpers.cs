fc3572adcee8dc26edc3fc599bb9027846006d02
Henry <henry.arroyo@ulatina.net>
2016-10-17 03:16:45 -0700

    Se agrego en la logica de negocios Juego, Jugador, ModalidadJuego y ModalidadesDeJuego y un Helper

---
+++ LogicaNegocios/Helpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocios
+{
+    public static class Helpers
+    {
+        public static bool sonCartonesIguales(CartonBingo carton1, CartonBingo carton2)
+        {
+          //  int[] array; 
+            for (int i=0; i<5; i++)
+            {
+              //  if(carton1.getCarton().Values.[i] != carton2.getCarton().Values[i])
+               // {
+               //     return false;
+              //  }
+            }
+            return true;;
+        }
+    }
+
+}
