1494df0499ed6139756ccc513f9efb8c3fd98b84
Henry <henry.arroyo@ulatina.net>
2016-10-17 10:37:39 -0700

    Se agregaron las propiedades y el constructor a la clase juego

--- LogicaNegocios/Juego.cs
+++ LogicaNegocios/Juego.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace LogicaNegocios
 {
-    class Juego
+    public class Juego
     {
+        public int totalNumeros { get; set; }
+        public int totalJugadores { get; set; }
+        public Jugador[] jugadores { get; set; }
+        public int intervalo { get; set; }
+
+        public Juego(int totalNumeros, int totalJugadores)
+        {
+            Console.WriteLine("JUEGO INICIO");
+            this.totalNumeros = totalNumeros;
+            this.intervalo = (int)(totalNumeros / 5);
+        }
     }
 }
