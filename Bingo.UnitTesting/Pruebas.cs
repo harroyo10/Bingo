0abb857c14a60df507032acb420b8566d8ac4c1b
1001140292 <1001140292@1001140292-PC>
2016-10-19 12:16:26 -0600

    CambiosUnitTesting

---
+++ Bingo.UnitTesting/Pruebas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogicaNegocios;
+
+
+namespace Bingo.UnitTesting
+{
+    [TestClass]
+    public class Pruebas
+    {
+        [TestMethod]
+        public void UnGanador()
+        {
+
+        }
+
+        [TestMethod]
+        public void VariosGanadores()
+        {
+
+        }
+
+        [TestMethod]
+        public void NingunGanador()
+        {
+
+        }  [TestMethod]
+        public void ElNumeroYaSalio()
+        {
+           /* int ElNumero = 0;
+            int ElIntervalo = 0;
+            bool Resultado = false;
+
+            HerramientasJuego ReferenciaDinamica = new HerramientasJuego();
+            bool ResultadoReal = ReferenciaDinamica.(ElNumero,ElIntervalo);
+            Assert.AreEqual(Resultado, ResultadoReal);
+            */
+        }
+        [TestMethod]
+        public void ElNumeroNoHaSalido()
+        {
+            /* int ElNumero = 0;
+           int ElIntervalo = 0;
+           bool Resultado = false;
+
+           HerramientasJuego ReferenciaDinamica = new HerramientasJuego();
+           bool ResultadoReal = ReferenciaDinamica.(ElNumero,ElIntervalo);
+           Assert.AreNotEqual(Resultado, ResultadoReal);
+           */
+        }
+
+        [TestMethod]
+        public void NumeroEnOtraColumna()
+        {
+
+        }
+
+    }
+}
