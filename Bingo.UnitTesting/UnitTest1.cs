5a2512908032fc3352bffeaa85237ebd4902f3aa
1001140292 <1001140292@1001140292-PC>
2016-10-19 11:40:41 -0600

    Se agrega el Unit Testing

---
+++ Bingo.UnitTesting/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Bingo.UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void UnGanador()
+        {
+
+        }
+
+        [TestMethod]
+        public void VariosGanadores()
+        {
+
+        }
+
+        [TestMethod]
+        public void NingunGanador()
+        {
+
+        }  [TestMethod]
+        public void ElNumeroYaSalio()
+        {
+
+        }
+        [TestMethod]
+        public void ElNumeroNoHaSalido()
+        {
+
+        }
+
+        [TestMethod]
+        public void NumeroEnOtraColumna()
+        {
+
+        }
+
+    }
+}
