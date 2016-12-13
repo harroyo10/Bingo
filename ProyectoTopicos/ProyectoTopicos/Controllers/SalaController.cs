a0d6aeff91c525af280f677d52a7642a43fe30fa
Henry <henry.arroyo@ulatina.net>
2016-12-13 15:02:59 -0600

    customized names for controllers and models

--- ProyectoTopicos/ProyectoTopicos/Controllers/SalaController.cs
+++ ProyectoTopicos/ProyectoTopicos/Controllers/SalaController.cs
@@ -10,24 +10,24 @@ using ProyectoTopicos.Models;
 
 namespace ProyectoTopicos.Controllers
 {
-    public class RoomController : Controller
+    public class SalaController : Controller
     {
-        private RoomDBContext db = new RoomDBContext();
+        private SalaDBContext db = new SalaDBContext();
 
-        // GET: Room
+        // GET: Sala
         public ActionResult Index()
         {
             return View(db.Movies.ToList());
         }
 
-        // GET: Room/Details/5
-        public ActionResult Details(int? id)
+        // GET: Sala/Detalle/5
+        public ActionResult Detalle(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Room room = db.Movies.Find(id);
+            Sala room = db.Movies.Find(id);
             if (room == null)
             {
                 return HttpNotFound();
@@ -35,18 +35,18 @@ namespace ProyectoTopicos.Controllers
             return View(room);
         }
 
-        // GET: Room/Create
-        public ActionResult Create()
+        // GET: Sala/Crear
+        public ActionResult Crear()
         {
             return View();
         }
 
-        // POST: Room/Create
+        // POST: Sala/Crear
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "RoomID,RoomName,RoomCategory,RoomDisponibility")] Room room)
+        public ActionResult Crear([Bind(Include = "SalaID,SalaNombre,SalaCaracteristicas,SalaCantidadAsientos")] Sala room)
         {
             if (ModelState.IsValid)
             {
@@ -58,14 +58,14 @@ namespace ProyectoTopicos.Controllers
             return View(room);
         }
 
-        // GET: Room/Edit/5
-        public ActionResult Edit(int? id)
+        // GET: Sala/Modificar/5
+        public ActionResult Modificar(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Room room = db.Movies.Find(id);
+            Sala room = db.Movies.Find(id);
             if (room == null)
             {
                 return HttpNotFound();
@@ -73,12 +73,12 @@ namespace ProyectoTopicos.Controllers
             return View(room);
         }
 
-        // POST: Room/Edit/5
+        // POST: Sala/Modificar/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "RoomID,RoomName,RoomCategory,RoomDisponibility")] Room room)
+        public ActionResult Modificar([Bind(Include = "SalaID,SalaNombre,SalaCaracteristicas,SalaCantidadAsientos")] Sala room)
         {
             if (ModelState.IsValid)
             {
@@ -89,14 +89,14 @@ namespace ProyectoTopicos.Controllers
             return View(room);
         }
 
-        // GET: Room/Delete/5
-        public ActionResult Delete(int? id)
+        // GET: Sala/Eliminar/5
+        public ActionResult Eliminar(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Room room = db.Movies.Find(id);
+            Sala room = db.Movies.Find(id);
             if (room == null)
             {
                 return HttpNotFound();
@@ -104,12 +104,12 @@ namespace ProyectoTopicos.Controllers
             return View(room);
         }
 
-        // POST: Room/Delete/5
-        [HttpPost, ActionName("Delete")]
+        // POST: Sala/Eliminar/5
+        [HttpPost, ActionName("Eliminar")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult ConfirmarEliminado(int id)
         {
-            Room room = db.Movies.Find(id);
+            Sala room = db.Movies.Find(id);
             db.Movies.Remove(room);
             db.SaveChanges();
             return RedirectToAction("Index");
---
+++ ProyectoTopicos/ProyectoTopicos/Models/Sala.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoTopicos.Models
+{
+    public class Sala
+    {
+        [Display(Name = "ID")]
+        public int SalaID { get; set; }
+
+        [Display(Name = "Nombre de Sala")]
+        [Required(ErrorMessage = "Nombre es requerido")]
+        public string SalaNombre { get; set; }
+
+        [Display(Name = "Características de Sala")]
+        public string SalaCaracteristicas { get; set; }
+
+        [Display(Name = "Cantidad de Asientos")]
+        [Required(ErrorMessage = "Cantidad de asientos es requerido")]
+        public int SalaCantidadAsientos { get; set; }
+
+    }
+
+    public class SalaDBContext : DbContext
+    {
+        public DbSet<Sala> Movies { get; set; }
+    }
+}
